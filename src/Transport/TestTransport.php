zenstruck/messenger-test

commit 13376bb43a9aad4c3b8f698698b52d5e8d27cc2a

[minor] give a name to the transport in the Worker (#34)

src/Transport/TestTransport.php
@@ -151,7 +151,7 @@ final class TestTransport implements TransportInterface
             );
         }
 
-        $worker = new Worker([$this], $this->bus, $this->dispatcher);
+        $worker = new Worker([$this->name => $this], $this->bus, $this->dispatcher);
         $worker->run(['sleep' => 0]);
 
         // remove added listeners/subscribers

tests/Fixture/config/multi_transport.yaml
@@ -4,8 +4,14 @@ imports:
 framework:
     messenger:
         transports:
-            async1: test://
-            async2: test://?intercept=false&catch_exceptions=false&test_serialization=false
+            async1:
+                dsn: test://
+                retry_strategy:
+                    max_retries: 0
+            async2:
+                dsn: test://?intercept=false&catch_exceptions=false&test_serialization=false
+                retry_strategy:
+                    max_retries: 0
             async3: in-memory://
         routing:
             Zenstruck\Messenger\Test\Tests\Fixture\Messenger\MessageA: [async1]

tests/Fixture/config/single_transport.yaml
@@ -4,7 +4,10 @@ imports:
 framework:
     messenger:
         transports:
-            async: test://
+            async:
+                dsn: test://
+                retry_strategy:
+                    max_retries: 0
         routing:
             Zenstruck\Messenger\Test\Tests\Fixture\Messenger\MessageA: [async]
             Zenstruck\Messenger\Test\Tests\Fixture\Messenger\MessageB: [async]

commit 1381f279dc4b07c475474113fa086606704319af

[bug] disable retries
This fixes a BC break introduced in #34. That PR fixed an issue where
the transport names were not being properly registered with the Worker.
This enabled the retry system but broke code that was not expecting
retries to be made.

src/Transport/TestTransport.php
@@ -9,6 +9,7 @@ use Symfony\Component\Messenger\Event\WorkerMessageFailedEvent;
 use Symfony\Component\Messenger\Event\WorkerRunningEvent;
 use Symfony\Component\Messenger\EventListener\StopWorkerOnMessageLimitListener;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\RedeliveryStamp;
 use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
 use Symfony\Component\Messenger\Transport\TransportInterface;
 use Symfony\Component\Messenger\Worker;
@@ -228,6 +229,11 @@ final class TestTransport implements TransportInterface
 
     public function send(Envelope $envelope): Envelope
     {
+        if ($envelope->last(RedeliveryStamp::class)) {
+            // message is being retried, don't process
+            return $envelope;
+        }
+
         if ($this->shouldTestSerialization()) {
             Assert::try(
                 fn() => $this->serializer->decode($this->serializer->encode($envelope)),

tests/Fixture/config/multi_transport.yaml
@@ -6,12 +6,8 @@ framework:
         transports:
             async1:
                 dsn: test://
-                retry_strategy:
-                    max_retries: 0
             async2:
                 dsn: test://?intercept=false&catch_exceptions=false&test_serialization=false
-                retry_strategy:
-                    max_retries: 0
             async3: in-memory://
         routing:
             Zenstruck\Messenger\Test\Tests\Fixture\Messenger\MessageA: [async1]

tests/Fixture/config/single_transport.yaml
@@ -6,8 +6,6 @@ framework:
         transports:
             async:
                 dsn: test://
-                retry_strategy:
-                    max_retries: 0
         routing:
             Zenstruck\Messenger\Test\Tests\Fixture\Messenger\MessageA: [async]
             Zenstruck\Messenger\Test\Tests\Fixture\Messenger\MessageB: [async]