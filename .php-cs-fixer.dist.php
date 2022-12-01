885fbd86e43d7f9c798f34e9833bb8fea3f9e22d
Kevin Bond <kevinbond@gmail.com>
2022-12-01 12:19:08 -0500

    ci: adjust

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -41,7 +41,7 @@ return $config
             'remove_inheritdoc' => true,
         ],
         'phpdoc_to_comment' => false,
-        'function_declaration' => ['closure_function_spacing' => 'none'],
+        'function_declaration' => ['closure_function_spacing' => 'none', 'closure_fn_spacing' => 'none'],
         'nullable_type_declaration_for_default_null_value' => true,
         'phpdoc_separation' => ['groups' => [
             ['test', 'dataProvider'],
